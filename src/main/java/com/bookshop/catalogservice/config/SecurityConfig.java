c46332368b7057f771cda4faca87fce87e02f51c
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-03-12 11:45:54 +0100

    Only users with employee role can perform requests other than GET books

--- src/main/java/com/bookshop/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/bookshop/catalogservice/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .mvcMatchers(HttpMethod.GET, "/", "/books/**")
                         .permitAll() // Allow fetching books (GET) without authentication
-                        .anyRequest().authenticated())// Any other request should be authenticated
+                        .anyRequest().hasRole("employee"))// Any other request should be authenticated
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt) // Enable OAuth2 Resource server support using JWT authentication
                 .sessionManagement(sessionManagement ->  sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Each request needs to include the access token. Stateless behavior and no need to keep the user session alive between requests
                 .csrf(AbstractHttpConfigurer::disable) // Stateless authentication --> No need to use CSRF protection
