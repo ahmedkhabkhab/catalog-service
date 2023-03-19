e9dbd62570c7348c03caa82c4777823e5224ed9f
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-03-19 13:12:25 +0100

    Fix integration test

--- src/test/java/com/bookshop/catalogservice/domain/BookRepositoryJdbcTests.java
+++ src/test/java/com/bookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -1,33 +1,70 @@
 package com.bookshop.catalogservice.domain;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import com.bookshop.catalogservice.config.DataConfig;
-import com.bookshop.catalogservice.domain.Book;
-import com.bookshop.catalogservice.domain.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJdbcTest
 @Import(DataConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("integration")
-public class BookRepositoryJdbcTests {
+class BookRepositoryJdbcTests {
 
     @Autowired
     private BookRepository bookRepository;
     @Autowired
     private JdbcAggregateTemplate jdbcAggregateTemplate;
+    @Container
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+
+    @DynamicPropertySource
+    public static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", BookRepositoryJdbcTests::jdbcUrl);
+        registry.add("spring.datasource.username", postgresql::getUsername);
+        registry.add("spring.datasource.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String jdbcUrl() {
+        return String.format("jdbc:postgresql://%s:%s/%s",
+                postgresql.getContainerIpAddress(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName());
+    }
 
     @Test
-    public void findBookByIsbnWhenExisting() {
+    void findAllBooks() {
+        var book1 = Book.of("1234561235", "Title", "Author", 12.90, "Polarsophia");
+        var book2 = Book.of("1234561236", "Another Title", "Author", 12.90, "Polarsophia");
+        jdbcAggregateTemplate.insert(book1);
+        jdbcAggregateTemplate.insert(book2);
+
+        Iterable<Book> actualBooks = bookRepository.findAll();
+
+        assertThat(StreamSupport.stream(actualBooks.spliterator(), true)
+                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2.isbn()))
+                .collect(Collectors.toList())).hasSize(2);
+    }
+
+    @Test
+    void findBookByIsbnWhenExisting() {
         var bookIsbn = "1234561237";
         var book = Book.of(bookIsbn, "Title", "Author", 12.90, "Polarsophia");
         jdbcAggregateTemplate.insert(book);
@@ -36,6 +73,59 @@ public class BookRepositoryJdbcTests {
 
         assertThat(actualBook).isPresent();
         assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+
+    @Test
+    void findBookByIsbnWhenNotExisting() {
+        Optional<Book> actualBook = bookRepository.findByIsbn("1234561238");
+        assertThat(actualBook).isEmpty();
+    }
+
+    @Test
+    void existsByIsbnWhenExisting() {
+        var bookIsbn = "1234561239";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 12.90, "Polarsophia");
+        jdbcAggregateTemplate.insert(bookToCreate);
+
+        boolean existing = bookRepository.existsByIsbn(bookIsbn);
+
+        assertThat(existing).isTrue();
+    }
+
+    @Test
+    void existsByIsbnWhenNotExisting() {
+        boolean existing = bookRepository.existsByIsbn("1234561240");
+        assertThat(existing).isFalse();
+    }
+
+    @Test
+    void whenCreateBookNotAuthenticatedThenNoAuditMetadata() {
+        var bookToCreate = Book.of("1232343456", "Title", "Author", 12.90, "Polarsophia");
+        var createdBook = bookRepository.save(bookToCreate);
+
+        assertThat(createdBook.createdBy()).isNull();
+        assertThat(createdBook.lastModifiedBy()).isNull();
+    }
+
+    @Test
+    @WithMockUser("john")
+    void whenCreateBookAuthenticatedThenAuditMetadata() {
+        var bookToCreate = Book.of("1232343457", "Title", "Author", 12.90, "Polarsophia");
+        var createdBook = bookRepository.save(bookToCreate);
+
+        assertThat(createdBook.createdBy()).isEqualTo("john");
+        assertThat(createdBook.lastModifiedBy()).isEqualTo("john");
+    }
+
+    @Test
+    void deleteByIsbn() {
+        var bookIsbn = "1234561241";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 12.90, "Polarsophia");
+        var persistedBook = jdbcAggregateTemplate.insert(bookToCreate);
+
+        bookRepository.deleteByIsbn(bookIsbn);
+
+        assertThat(jdbcAggregateTemplate.findById(persistedBook.id(), Book.class)).isNull();
     }
 
 }
